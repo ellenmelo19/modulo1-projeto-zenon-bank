d1164bcac1387d174fd9d24f2af8914dd69b3df8
Ellen <ellen.melo156@gmail.com>
2026-07-20 20:07:20 -0300

    Adiciona projeto Maven zenon-fraud-detector com estrutura inicial.
    Cria o modulo Java com groupId br.com.zenon, artifactId
    zenon-fraud-detector, Java 25 e classe Main de entrada.
    O dataset PaySim permanece em data/ (fora do versionamento).

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
@@ -0,0 +1,11 @@
+package br.com.zenon;
+
+/**
+ * Ponto de entrada do detector de fraudes do Zenón Bank.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Zenón Fraud Detector - projeto inicializado com sucesso!");
+    }
+}
